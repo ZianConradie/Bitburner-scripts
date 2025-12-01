21e00d4a4179694c766b806f09325b242dd42af7
Zian Conradie <zianconradie@gmail.com>
2025-12-01 14:14:34 +0200

    Create homehack.js

---
+++ homehack.js
@@ -0,0 +1,12 @@
+/** @param {NS} ns */
+export async function main(ns) {
+    const home = "home";
+    const script = "hack.js";
+
+    const selfRam = ns.getScriptRam(ns.getScriptName());
+    const maxRam = ns.getServerMaxRam(home) - selfRam;
+    const scriptRam = ns.getScriptRam(script);
+    const threads = Math.floor(maxRam / scriptRam);
+
+    ns.exec("hack.js", "home", threads-1)
+}
