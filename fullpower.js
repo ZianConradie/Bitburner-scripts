3769520439cf8f57853799fad3738ee30453663d
ChkChkChkBoom <1076756@lwsd.org>
2025-12-06 18:25:01 -0800

    Create fullpower.js

---
+++ fullpower.js
@@ -0,0 +1,20 @@
+/** @param {NS} ns */
+export async function main(ns) {
+     // Where to send to
+    const target = ns.args[0]
+    // What to send
+    const send = ns.args[1]
+    // How much it costs
+    const cost = ns.args[2]
+    // Params of sent script
+    let args=[]
+    const len = ns.args.length
+    for (let i=3;i<len;i++){
+        args.push(ns.args[i])
+    }
+    const amnt = Math.floor((ns.getServerMaxRam(target)-ns.getServerUsedRam(target))/cost)
+    if (amnt>0){
+        ns.scp(send,target,"home")
+        ns.exec(send,target,amnt,args)
+    }
+}
